2404dcb83ed8eb7278bbf22173a7fb84bea4db90
ashok <ashok@Ashok>
2022-09-29 00:23:19 -0700

    first commit uploading all files

---
+++ String program/src/RevString.java
@@ -0,0 +1,16 @@
+
+public class RevString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+String a="ashok";
+String b="ashok";
+String c=new String("ashok");
+System.out.println(a==b);
+System.out.println(a.equals(c));
+System.out.println(a==c);
+	}
+
+
+}
